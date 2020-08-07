fc2001912470679b165f2670495dafeee701305f
heymands <supmands@gmail.com>
2020-08-06 23:01:39 -0300

    update

--- web/src/components/TeacherItem/index.tsx
+++ web/src/components/TeacherItem/index.tsx
@@ -3,36 +3,56 @@ import React from 'react';
 import whatsappIcon from '../../assets/images/icons/whatsapp.svg';
 
 import './styles.css';
+import api from '../../services/api';
 
-function TeacherItem() {
+export interface Teacher {
+  id: number;
+  avatar: string;
+  bio: string;
+  cost: number;
+  name: string;
+  subject: string;
+  whatsapp: string;
+}
+interface TeacherItemProps {
+  teacher: Teacher;
+
+};
+
+const TeacherItem: React.FC <TeacherItemProps> = ({ teacher }) => {
+  function createNewConnection() {
+    api.post('connections', {
+      user_id: teacher.id, 
+    });
+  }
+
   return (
     <article className="teacher-item">
       <header>
-        <img src="https://pbs.twimg.com/profile_images/900428598456315905/p7pD-4GT_400x400.jpg" alt="Amanda da Costa"/>
+        <img src={teacher.avatar} alt={teacher.name}/>
         <div>
-          <strong>Amanda da Costa</strong>
-          <span>História</span>
+          <strong>{teacher.name}</strong>
+          <span>{teacher.subject}</span>
         </div>
       </header>
 
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus
-        quis excepturi vel repellendus corrupti eos, labore natus, harum
-        <br /><br />
-        dicta expedita cumque nihil quod. Ipsum tempora, illo explicabo 
-        repellat autem accusantium.
-      </p>
+      <p> {teacher.bio} </p>
 
       <footer>
         <p>
           Preço / Hora
-          <strong>R$ 80,00</strong>
+          <strong>R$ {teacher.cost}</strong>
         </p>
 
-        <button type="button">
+        <a 
+          target="_blank" 
+          onClick={createNewConnection} 
+          href={`https://wa.me/${teacher.whatsapp}`
+        }>
+          
         <img src={whatsappIcon} alt="whatsapp"/>
         Entrar em Contato
-        </button>
+        </a>
       </footer>
     </article>
   );
--- web/src/pages/Landing/index.tsx
+++ web/src/pages/Landing/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 import { Link } from 'react-router-dom';
 
@@ -8,10 +8,23 @@ import landingImg from '../../assets/images/landing.svg'
 import studyIcon from '../../assets/images/icons/study.svg'
 import giveClassIcon from '../../assets/images/icons/give-classes.svg'
 import purpleHeartIcon from '../../assets/images/icons/purple-heart.svg'
+import api from '../../services/api';
 
 import './styles.css';
 
+
 function Landing() {
+
+   const [totalConnections, setTotalConnections] = useState(0);
+   
+   useEffect(() => {
+     api.get('connections').then(response =>{
+       const { total } = response.data;
+       setTotalConnections(total);
+     })
+   }, []);
+
+
   return (
     <div id="page-landing">
 
@@ -19,11 +32,11 @@ function Landing() {
 
         <div className="logo-container">
 
-          <img src={logoImg} alt="Proffy"/>
+          <img src={ logoImg } alt="Proffy"/>
           <h2>Sua plataforma de estudos online.</h2>
         </div>
 
-        <img src={landingImg} 
+        <img src={ landingImg } 
         alt="Plataforma de estudos" 
         className="hero-image"
         />
@@ -31,7 +44,7 @@ function Landing() {
         <div className="buttons-container">
 
           <Link to="/study" className="study">
-            <img src={studyIcon} alt="Estudar"/>
+            <img src={ studyIcon } alt="Estudar"/>
             Estudar
           </Link>
 
@@ -43,7 +56,7 @@ function Landing() {
         </div>
 
         <span className="total-connections">
-          Total de 200 conexões já realizadas <img src={purpleHeartIcon} alt="Coração Roxo"/>
+          Total de { totalConnections } conexões já realizadas <img src={purpleHeartIcon} alt="Coração Roxo"/>
         </span>
       </div>
     </div>
